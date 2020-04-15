96f04d60a0bb1911bd9f2eab7070f28e145b3613
Ria Gupta <ria18405@iiitd.ac.in>
2020-04-16 03:20:24 +0530

    Comments added

--- script.py
+++ script.py
@@ -2,9 +2,8 @@ from sklearn import svm
 
 def find_patterns(n,p):
 	"""
-		divides protien sequence p string into patterns of length n
+		divides training set protien sequence p string into patterns of length n
 		output = 2 matrices = X and Y for train.data
-		and only X for test.data
 	"""
 	X=[]
 	sequences=[]
@@ -23,27 +22,26 @@ def find_patterns(n,p):
 			elif (chr(ord(seq[j])-32) in dict):
 				dict[chr(ord(seq[j])-32)]=1
 			seq_dict.extend(list(dict.values()))
-		# print(seq)
-		# print(seq_dict)
+
 		X.append(seq_dict)
 	
+	"""
+	Small alphabet indicates ATP interacting residues
+	ATP interacting = +1
+	Non ATP interacting = -1
 
+	"""
 		if (ord(p[i:i+n][half_n]) >=97):
 			Y.append(1)
 		else:
 			Y.append(-1)
-	# print(X)
-	# print(Y)
+
 	return X,Y
-	# print(sequences)
-	for p in sequences:
-		if (len(p)!=7):
-			print("error",p)
+
 def find_patterns_test(n,p):
 	"""
 		divides protien sequence p string into patterns of length n
-		output = 2 matrices = X and Y for train.data
-		and only X for test.data
+		output = 1 matrix = X for test.data
 	"""
 	X=[]
 	sequences=[]
@@ -58,68 +56,76 @@ def find_patterns_test(n,p):
 			if (seq[j] in dict):
 				dict[seq[j]]=1 
 			seq_dict.extend(list(dict.values()))
-		# print(seq)
-		# print(seq_dict)
+
 		X.append(seq_dict)
 		
 	return X
-	# print(sequences)
+
+
+#-------------TRAINING------------------
 X_train=[]
 Y_train=[]
 file=open('train.data','r')
 data=file.readlines()
-n=7
+n=7			#n=pattern length
 for i in range(1,len(data)):
+	
+	"""
+	Addition of 'XXX..XX' at the beginning and end of protien sequences
+	(n-1)/2 number of X are added in both directions
+	Output : s->train-protien_sequence
+	"""
 	s=''
 	count_x_insert=(n-1)//2
 	s+='X'*count_x_insert
 	s+=data[i][8:-1]
-	s+='X'*count_x_insert
-	# print(s)
+	s+='X'*count_x_insert		
+
+
 	X,Y=find_patterns(n,s)
 	X_train.extend(X)
 	Y_train.extend(Y)
 
 
-
-
-# print(X_train)
-# print(Y_train)
-
-# print(len(X_train),len(Y_train))
-
 """
-now apply five fold verification: append 4 outputs to X and 
+[Optional]now apply five fold verification: append 4 outputs to X and 
 leave 1 for testing purpose.
 """
 
 clf = svm.SVC(gamma ='scale')
 clf.fit(X_train, Y_train)
 
-
-# print(X)
-# print(Y)
-# print(len(X),len(Y))
-
+#-------------------TESTING-----------
 
 file=open('test1.txt','r')
 data=file.readlines()
+
+"""
+Addition of 'XXX..XX' at the beginning and end of protien sequences
+(n-1)/2 number of X are added in both directions
+Output : s-test-protien_sequence
+"""
+
 s=''	
 count_x_insert=(n-1)//2
 s+='X'*count_x_insert
 for i in range(1,len(data)):
 	s+=data[i][6]
 
-s+='X'*count_x_insert
+s+='X'*count_x_insert		
+
 
 X_test=find_patterns_test(n,s)
-# print(X)
-# print(len(X))
-# print(s)			#s=test_protien_seq
+
+#making predictions
 out=clf.predict(X_test)
-print(out)
 
 
+
+"""
+Writing predictions output in a file output.txt
+with the respective Protien IDs
+"""
 ofile = open("output.txt","w")
 s="ID,Lable\n"
 ofile.write(s)
@@ -131,7 +137,6 @@ data = data[1:]
 for line in data:
 	s = list(line.split(","))
 	l = s[0]+","+str(out[ind])+'\n'
-	# if (out[ind]==1):
-	# 	print("1 found")
+
 	ofile.write(l)
 	ind=ind+1
